6f71231a31f9850e633e9458026ae764f6020616
Borko <bor.kos.mk@gmail.com>
2023-02-21 13:55:26 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+      <?php
+
+      require('includes/app.php');
+
+      $database = new Dbh();
+      $db = $database->connect();
+
+      $post = new Post($db);
+      $products = $post->read();
+    ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,8 +16,8 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Document</title>
-  <link rel="stylesheet" href="style/normalize.css" />
   <link rel="stylesheet" href="style/style.css" />
+  <link rel="stylesheet" href="style/normalize.css" />
 </head>
 
 <body>
@@ -19,36 +29,30 @@
     </nav>
   </header>
   <section class="product-list-wrapper">
-    <form method="POST" action="delete.php" id="delete_form">
-
-      <?php
-
-      require('includes/app.php');
-
-      $database = new Dbh();
-      $db = $database->connect();
-
-      $post = new Post($db);
-      $products = $post->read();
+       
+    
+<form method="POST" action="delete.php" id="delete_form">
+    <?php foreach ($products as $product) : ?>
+      <table>
       
-      if(!empty($products)){
-      foreach ($products as $product) {
-
-        echo "<table>";
-        echo        "<tbody>";
-        echo         "<tr class='content'>";
-        echo          "<th> <input type='checkbox' name='checkbox[]' value='" . $product->getId()  ."'> </th>";
-        echo            "<td>" .  $product->getSku() . "</td>";
-        echo            "<td>" .  $product->getName() . "</td>";
-        echo            "<td>" .  $product->getPrice() . "</td>";
-        echo            "<td>"  . implode(', ', $product->getAdditionalProperties()) . "</td>";
-        echo           "</tr>";
-        echo        "</tbody>";
-        echo      "</table>";
-      }
-    }
-      ?>
-    </form>
+        <tbody>
+             
+          <tr class='content'>
+             
+            <th>
+             <input type='checkbox' class='delete-checkbox' name='checkbox[]' value="<?= $product->getId() ?>">
+            </th>
+            <td> <?= $product->getSku() ?> </td>
+            <td> <?=  $product->getName() ?> </td>
+            <td> <?=  $product->getPrice() ?> </td>
+            <td> <?= implode(', ', $product->getAdditionalProperties()) ?> </td>
+          </tr>
+            
+        </tbody>
+       
+      </table>
+ <?php endforeach; ?>
+</form>
   </section>
   <footer class="footer-wrap">
     <div class="footer">
