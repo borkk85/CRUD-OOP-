44eecf08c6125b042549379edcc06a62fa86c6de
Borko <bor.kos.mk@gmail.com>
2023-02-21 13:51:19 +0100

    Update product.class.php

--- classes/product.class.php
+++ classes/product.class.php
@@ -6,14 +6,18 @@ error_reporting(E_ALL);
 
 class Product implements ProductInterface
 {
+
+    use CommonTrait;
+
+
     protected $id;
     protected $sku;
     protected $name;
     protected $price;
     protected $productType;
-    protected $additionalProperties;
+    protected $additionalProperties = [];
 
-    public function __construct($id, $sku, $name, $price, $productType, array $additionalProperties)
+    public function __construct($id, $sku, $name, $price, $productType, array $additionalProperties = [])
     {
         $this->id = $id;
         $this->sku = $sku;
@@ -32,7 +36,7 @@ class Product implements ProductInterface
 
     public function getId()
     {
-       return $this->id;
+        return $this->id;
     }
 
     public function getSku()
@@ -57,7 +61,7 @@ class Product implements ProductInterface
 
     public function getPrice()
     {
-        return $this->price;
+        return "$this->price $";
     }
 
     public function setPrice($price)
@@ -65,32 +69,21 @@ class Product implements ProductInterface
         $this->price = $price;
     }
 
-    public function setAdditionalProperties($additionalProperties)
-    {
-        $this->additionalProperties = $additionalProperties;
-    }
 
-    public function getAdditionalProperties()
+    public static function displayProduct($row)
     {
-        return $this->additionalProperties;
-    }
-
-    public static function createProduct($row) {
-        $id = $row['id'];
         $productType = $row['productType'];
-        $additionalProperties = [];
-        if ($productType == 'DVD') {
-            $product = new self(
+        if ($productType == 'dvd') {
+            $product = new Dvd(
                 $row['id'],
                 $row['sku'],
                 $row['name'],
                 $row['price'],
                 $row['productType'],
-                $row['size']
             );
-            $additionalProperties = ['Size: ' . $row['size']];
-        } elseif ($productType == 'Book') {
-            $product = new self(
+            $product->setAdditionalProperties(['Size: ' . $row['size'] . 'MB']);
+        } elseif ($productType == 'book') {
+            $product = new Book(
                 $row['id'],
                 $row['sku'],
                 $row['name'],
@@ -98,9 +91,9 @@ class Product implements ProductInterface
                 $row['productType'],
                 $row['weight']
             );
-            $additionalProperties = ['Weight: ' . $row['weight']];
-        } elseif ($productType == 'Furniture') {
-            $product = new self(
+            $product->setAdditionalProperties(['Weight: ' . $row['weight'] . 'KG']);
+        } elseif ($productType == 'furniture') {
+            $product = new Fur(
                 $row['id'],
                 $row['sku'],
                 $row['name'],
@@ -110,13 +103,12 @@ class Product implements ProductInterface
                 $row['length'],
                 $row['width']
             );
-            $additionalProperties = [
-                'Height: ' . $row['height'],
-                'Length: ' . $row['length'],
-                'Width: ' . $row['width'],
-            ];
+            $product->setAdditionalProperties([
+                'Dimension: ' . $row['height'] . 'x'
+                    . $row['length'] . 'x' . $row['width']
+            ]);
         } else {
-            $product = new self(
+            $product = new Product(
                 $row['id'],
                 $row['sku'],
                 $row['name'],
@@ -124,22 +116,12 @@ class Product implements ProductInterface
                 $row['productType'],
                 []
             );
-            if ($row['size']) {
-                $additionalProperties[] = 'Size: ' . $row['size'];
-            }
-            if ($row['weight']) {
-                $additionalProperties[] = 'Weight: ' . $row['weight'];
-            }
-            if ($row['height'] && $row['length'] && $row['width']) {
-                $additionalProperties = [
-                    'Height: ' . $row['height'] . '</br>' .
-                    'Length: ' . $row['length'] . '</br>' .
-                    'Width: ' . $row['width']];
-                }
-                $product->setAdditionalProperties($additionalProperties);
-                return $product;
+            
         }
-}
+        // var_dump($product);
+        return $product;
+        // print_r($product);
+    }
 }
 
 interface ProductInterface
@@ -148,43 +130,15 @@ interface ProductInterface
     public function getAdditionalProperties();
 }
 
-
-trait DvdTrait
+trait CommonTrait
 {
     public function setAdditionalProperties(array $additionalProperties)
     {
-        $this->additionalProperties = 
-        array_merge($this->additionalProperties, ['Size: ' . $additionalProperties[0]]);
+        $this->additionalProperties =  $additionalProperties;
     }
+
     public function getAdditionalProperties(): array
     {
-        return $this->additionalProperties;
-    }
-}
-
-trait BookTrait
-{
-    public function setAdditionalProperties(array $additionalProperties)
-    {
-        $this->additionalProperties = 
-        array_merge($this->additionalProperties, ['Weight: ' . $additionalProperties[0]]);
-    }
-    public function getAdditionalProperties(): array
-    {
-        return $this->additionalProperties;
-    }
-}
-
-trait FurTrait
-{
-    public function setAdditionalProperties(array $additionalProperties) {
-        $this->additionalProperties = array_merge(
-            $this->additionalProperties,
-            ['Height: ' . $additionalProperties[0], 'Length: ' . $additionalProperties[1], 'Width: ' . $additionalProperties[2]]
-        );
-    }
-
-    public function getAdditionalProperties(): array {
         return $this->additionalProperties;
     }
 }
